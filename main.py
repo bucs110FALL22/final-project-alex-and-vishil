727020376ed27ee62ef4c41003744c4e8a6442ec
Alex Eskenazi <112673600+alexeskenazi@users.noreply.github.com>
2022-11-13 22:06:36 +0000

    Attempting to fix replit to work with pygame

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 import pygame
 from src.controller import Controller
 
+
 def main():
-  pygame.init()
-  controller = Controller()
-  controller.mainloop()
+	pygame.init()
+	surface = pygame.display.set_mode((1200, 1000))
+	controller = Controller()
+	controller.mainloop()
 
 
 # https://codefather.tech/blog/if-name-main-python/
 if __name__ == '__main__':
-  main()
+	main()
